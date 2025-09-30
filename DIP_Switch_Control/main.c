8a12172c39291b971b2c68ccbb9f29ce6daf57d7
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-09-30 10:49:09 +0900

    Update main.c

--- DIP_Switch_Control/main.c
+++ DIP_Switch_Control/main.c
@@ -26,9 +26,9 @@ int main(void) {
 		           | ( GPIO_READ(GPIO_PORTA, 0x40) >> 5 )   // PA6 → bit1 0000 0010
 		           | ( GPIO_READ(GPIO_PORTA, 0x80) >> 5 )   // PA7 → bit2 0000 0100
 		           | ( GPIO_READ(GPIO_PORTB, 0x08))   // PB3 → bit3 0000 1000
-		           | ( GPIO_READ(GPIO_PORTQ, 0x10) >> 0 )   // PQ4 → bit4
+		           | ( GPIO_READ(GPIO_PORTQ, 0x40) >> 2 )   // PQ4 → bit4
 		           | ( GPIO_READ(GPIO_PORTQ, 0x20) >> 0 )   // PQ5 → bit5
-		           | ( GPIO_READ(GPIO_PORTQ, 0x40) >> 0 )   // PQ6 → bit6
+		           | ( GPIO_READ(GPIO_PORTQ, 0x10) << 2 )   // PQ6 → bit6 0100 0000
 		           | ( GPIO_READ(GPIO_PORTG, 0x40) << 1 );  // PG6 → bit7
 		// LED
 		GPIO_WRITE(GPIO_PORTL, 0xf, (dip_data&0xf));
