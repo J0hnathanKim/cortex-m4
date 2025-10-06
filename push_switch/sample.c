ca98b4eeeadaf6a712789d115b3959688fd2c3d7
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-10-06 15:27:25 +0900

    Update sample.c

--- push_switch/sample.c
+++ push_switch/sample.c
@@ -21,7 +21,7 @@ int main(void) {
 	LED_clear();
 
 	while(1){
-		push_data = GPIO_READ(GPIO_PORTP,0x02) >> 1;
+		push_data = (~GPIO_READ(GPIO_PORTP, 0x02) >> 1) & (~GPIO_READ(GPIO_PORTN, 0x08) >> 2) & (~GPIO_READ(GPIO_PORTE, 0x20) >> 3) & (~GPIO_READ(GPIO_PORTK, 0x80) >> 4);
 		/*
 		if()
 			LED count decrease
