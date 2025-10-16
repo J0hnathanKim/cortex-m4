8b79b69ecdde5072f8223ed0b130ebb88178dcb3
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-10-16 21:11:09 +0900

    Create main1.c

---
+++ 7segment/task/main1.c
@@ -0,0 +1,57 @@
+//라이브러리는 기존꺼로 적용하기
+//내가 뭘 간과했는지 살펴보자.
+#include "cortex_m4.h"
+#include "MyLib.h"
+
+
+void delay(int count);
+
+int main(void) {
+	int push2_current;
+	int push2_prev = 0;
+
+
+	uint32_t ui32SysClock;
+	// Run from the PLL at 120 MHz.
+	ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL |
+											   SYSCTL_CFG_VCO_480), 120000000);
+    int start_flag = 0;
+	PUSH_init();
+	DIP_init();
+	FND_init();
+	FND_clear();
+
+	int nums[6] = {6,1,2,3,4,5};
+    int i, j, tmp;
+	while(1){
+      if(start_flag == 1){
+            for(i=0; i<6; i++){
+                WRITE_FND(i+1, nums[i]);
+            }
+	    }
+		push2_current = GPIO_READ(GPIO_PORTN, PIN3);
+
+		if(push2_prev != 0 && push2_current == 0) {
+
+			        tmp = nums[0];
+			        for(i=0; i<5; i++){
+			            nums[i] = nums[i+1];
+			        }
+			        nums[5] = tmp;
+			        start_flag = 1;
+		}
+
+		push2_prev = push2_current;
+
+		delay(10000);
+	}
+	return 0;
+}
+
+
+
+void delay(int count){
+	while(count != 0){
+		count--;
+	}
+}
