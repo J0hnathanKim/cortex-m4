928d2a1f64192c334da5b15bb01afbd1b368c41f
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-10-19 11:16:52 +0900

    Update count_up_down.c

--- LED_student/count_up_down.c
+++ LED_student/count_up_down.c
@@ -1,4 +1,4 @@
-#include "cortex_m4.h"
+/*#include "cortex_m4.h"
 #include "MyLib.h"
 
 void LED_clear();
@@ -43,4 +43,53 @@ void delay(int count){
 	while(count != 0){
 		count--;
 	}
+}*/
+
+#include "cortex_m4.h"
+#include "MyLib.h"
+
+void LED_clear(void);
+void delay(int);
+
+int main(void)
+{
+    uint32_t g_ui32SysClock;
+
+    g_ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+                                         SYSCTL_OSC_MAIN   |
+                                         SYSCTL_USE_PLL    |
+                                         SYSCTL_CFG_VCO_480),
+                                         120000000);
+
+    LED_init();
+    LED_clear();
+
+    int val = 0;
+    while (1) {
+        // up count from 1 to 15
+        for (val = 1; val <= 15; val++) {
+            GPIO_WRITE(GPIO_PORTL, 0x0F, val & 0x0F);
+            delay(2000000);
+        }
+
+        // down count from 14 to 1
+        for (val = 14; val >= 1; val--) {
+            GPIO_WRITE(GPIO_PORTL, 0x0F, val & 0x0F);
+            delay(2000000);
+        }
+    }
+
 }
+
+void LED_clear(){
+	GPIO_WRITE(GPIO_PORTL, 0xf, 0x0);
+	GPIO_WRITE(GPIO_PORTM, 0xf, 0x0);
+	delay(2500000);
+}
+
+void delay(int count){
+	while(count != 0){
+		count--;
+	}
+}
+
