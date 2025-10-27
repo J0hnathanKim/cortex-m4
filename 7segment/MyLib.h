97df8e28747b6b6c099a24f21236188fe6c72fea
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-10-27 23:44:12 +0900

    Update MyLib.h

--- 7segment/MyLib.h
+++ 7segment/MyLib.h
@@ -1,130 +1,53 @@
 /*
- * MyLib.c
+ * MyLib.h
  *
- *  Created on: 2018. 03. 08.
- *      Author: ESL-KMC
+ *  Created on: 2015. 4. 6.
+ *      Author: ESL-LKH
  */
-#include "MyLib.h"
-#include "cortex_m4.h"
 
-void FND_init(){
+#ifndef MYLIB_H_
+#define MYLIB_H_
+static unsigned char fnd_num[18] = {
+//	0		1		2		3		4		5		6		7
+	0x3f,	0x06,	0x5b,	0x4f,	0x66,	0x6d,	0x7d,	0x27,
+//	8		9		a		b		c		d		e		f
+	0x7f,	0x67,	0x5f,	0x7c,	0x39,	0x5e,	0x79,	0x71, 0x40,
+//	off
+	0x00
+};
 
-	RCGCGPIO = RCGCGPIO | 0x10C4;
+static unsigned char fnd_pos[6] = {
+//	D1		D2		D3		D4		D5		D6
+	0x01,	0x02,	0x04,	0x08,	0x10,	0x20
+};
 
-	GPIO_PORTH_DIR = GPIO_PORTH_DIR | 0b1111;
-	GPIO_PORTN_DIR = GPIO_PORTN_DIR | 0b110000;
-	GPIO_PORTC_DIR = GPIO_PORTC_DIR | 0b11110000;
-	GPIO_PORTG_DIR = GPIO_PORTG_DIR | 0b1111;
-
-	GPIO_PORTH_DEN = GPIO_PORTH_DEN | 0b1111;
-	GPIO_PORTN_DEN = GPIO_PORTN_DEN | 0b110000;
-	GPIO_PORTC_DEN = GPIO_PORTC_DEN | 0b11110000;
-	GPIO_PORTG_DEN = GPIO_PORTG_DEN | 0b1111;
-
-	GPIO_PORTH_AFSEL = GPIO_PORTH_AFSEL & (~ (0x0F));
-	GPIO_PORTN_AFSEL = GPIO_PORTN_AFSEL & (~ (0x30));
-	GPIO_PORTC_AFSEL = GPIO_PORTC_AFSEL & (~ (0xF0));
-	GPIO_PORTG_AFSEL = GPIO_PORTG_AFSEL & (~ (0x0F));
-
-	GPIO_PORTH_PC = GPIO_PORTH_PC | 0x00FF;
-	GPIO_PORTN_PC = GPIO_PORTN_PC | 0x0F00;
-	GPIO_PORTC_PC = GPIO_PORTC_PC | 0xFF00;
-	GPIO_PORTG_PC = GPIO_PORTG_PC | 0x00FF;
-
-	GPIO_PORTH_DR2R = GPIO_PORTH_DR2R & (~(0x0F));
-	GPIO_PORTN_DR2R = GPIO_PORTN_DR2R & (~(0x30));
-	GPIO_PORTC_DR2R = GPIO_PORTC_DR2R & (~(0xF0));
-	GPIO_PORTG_DR2R = GPIO_PORTG_DR2R & (~(0x0F));
-
-	GPIO_PORTH_DR4R = GPIO_PORTH_DR4R | 0x0F;
-	GPIO_PORTN_DR4R = GPIO_PORTN_DR4R | 0x30;
-	GPIO_PORTC_DR4R = GPIO_PORTC_DR4R | 0xF0;
-	GPIO_PORTG_DR4R = GPIO_PORTG_DR4R | 0x0F;
-
-	GPIO_PORTH_DR8R = GPIO_PORTH_DR8R | 0x0F;
-	GPIO_PORTN_DR8R = GPIO_PORTN_DR8R | 0x30;
-	GPIO_PORTC_DR8R = GPIO_PORTC_DR8R | 0xF0;
-	GPIO_PORTG_DR8R = GPIO_PORTG_DR8R | 0x0F;
-
-	GPIO_PORTH_SLR = GPIO_PORTH_SLR & (~(0x0F));
-	GPIO_PORTN_SLR = GPIO_PORTN_SLR & (~(0x30));
-	GPIO_PORTC_SLR = GPIO_PORTC_SLR & (~(0xF0));
-	GPIO_PORTG_SLR = GPIO_PORTG_SLR & (~(0x0F));
-
-	GPIO_PORTH_DR12R = GPIO_PORTH_DR12R & (~(0x0F));
-	GPIO_PORTN_DR12R = GPIO_PORTN_DR12R & (~(0x30));
-	GPIO_PORTC_DR12R = GPIO_PORTC_DR12R & (~(0xF0));
-	GPIO_PORTG_DR12R = GPIO_PORTG_DR12R & (~(0x0F));
-}
-
-void PUSH_init(){
-	RCGCGPIO = RCGCGPIO | 0x3E10;
-
-	GPIO_PORTP_DIR = GPIO_PORTP_DIR & ~(0x01<<1);
-	GPIO_PORTN_DIR = GPIO_PORTN_DIR & ~(0x01<<3);
-	GPIO_PORTE_DIR = GPIO_PORTE_DIR & ~(0x01<<5);
-	GPIO_PORTK_DIR = GPIO_PORTK_DIR & ~(0x01<<7);
-
-	GPIO_PORTP_AFSEL = GPIO_PORTP_AFSEL & (~ (0x01<<1));
-	GPIO_PORTN_AFSEL = GPIO_PORTN_AFSEL & (~ (0x01<<3));
-	GPIO_PORTE_AFSEL = GPIO_PORTE_AFSEL & (~ (0x01<<5));
-	GPIO_PORTK_AFSEL = GPIO_PORTK_AFSEL & (~ (0x01<<7));
-
-	GPIO_PORTP_DEN = GPIO_PORTP_DEN | (0x01<<1);
-	GPIO_PORTN_DEN = GPIO_PORTN_DEN | (0x01<<3);
-	GPIO_PORTE_DEN = GPIO_PORTE_DEN | (0x01<<5);
-	GPIO_PORTK_DEN = GPIO_PORTK_DEN | (0x01<<7);
-}
-void DIP_init(){
-	RCGCGPIO = RCGCGPIO | 0x4043;
-
-	GPIO_PORTA_DIR = GPIO_PORTA_DIR & ~((0x01<<3) | (0x01<<6) | (0x01<<7));
-	GPIO_PORTB_DIR = GPIO_PORTB_DIR & ~(0x01<<3);
-	GPIO_PORTQ_DIR = GPIO_PORTQ_DIR & ~((0x01<<6) | (0x01<<5) | (0x01<<4));
-	GPIO_PORTG_DIR = GPIO_PORTG_DIR & ~(0x01<<6);
-
-	GPIO_PORTA_AFSEL = GPIO_PORTA_AFSEL & (~ ((0x01<<3) | (0x01<<6) | (0x01<<7)));
-	GPIO_PORTB_AFSEL = GPIO_PORTB_AFSEL & (~ (0x01<<3));
-	GPIO_PORTQ_AFSEL = GPIO_PORTQ_AFSEL & (~ ((0x01<<6) | (0x01<<5) | (0x01<<4)));
-	GPIO_PORTG_AFSEL = GPIO_PORTG_AFSEL & (~ (0x01<<6));
-
-	GPIO_PORTA_DEN = GPIO_PORTA_DEN | (0x01<<3) | (0x01<<6) | (0x01<<7);
-	GPIO_PORTB_DEN = GPIO_PORTB_DEN | (0x01<<3);
-	GPIO_PORTQ_DEN = GPIO_PORTQ_DEN | (0x01<<6) | (0x01<<5) | (0x01<<4);
-	GPIO_PORTG_DEN = GPIO_PORTG_DEN | 0x01<<6;
-}
-
-void WRITE_FND(int digit, int seg_out){
-	int i;
-
-	GPIO_WRITE(GPIO_PORTC, 0x10 | 0x20 | 0x40 | 0x80, (fnd_num[seg_out] << 4) & 0xf0);
-	GPIO_WRITE(GPIO_PORTG, 0x01 | 0x02 | 0x04 | 0x08, (fnd_num[seg_out] >> 4) & 0x0f);
-
-	GPIO_WRITE(GPIO_PORTH, 0x01 | 0x02 | 0x04 | 0x08, fnd_pos[digit-1] & 0x0f);
-	GPIO_WRITE(GPIO_PORTN, 0x10 | 0x20, fnd_pos[digit-1] & 0xf0);
-
-	for(i=0; i<9999;i++);
-}
-
-void WRITE_FND_DOT(int digit, int seg_out){
-	int i;
-
-	GPIO_WRITE(GPIO_PORTC, 0x10 | 0x20 | 0x40 | 0x80,  (fnd_num[seg_out] << 4) & 0xf0);
-	GPIO_WRITE(GPIO_PORTG, 0x01 | 0x02 | 0x04 | 0x08, ((fnd_num[seg_out] >> 4) & 0x0f) | 0x08);
-
-	GPIO_WRITE(GPIO_PORTH, 0x01 | 0x02 | 0x04 | 0x08, fnd_pos[digit-1] & 0x0f);
-	GPIO_WRITE(GPIO_PORTN, 0x10 | 0x20, fnd_pos[digit-1] & 0xf0);
-
-	for(i=0; i<9999;i++);
-}
-
-void FND_clear(){
-	GPIO_WRITE(GPIO_PORTC, 0x10 | 0x20 | 0x40 | 0x80, 0);
-	GPIO_WRITE(GPIO_PORTG, 0x01 | 0x02 | 0x04 | 0x08, 0);
-
-	GPIO_WRITE(GPIO_PORTH, 0x01 | 0x02 | 0x04 | 0x08, 0);
-	GPIO_WRITE(GPIO_PORTN, 0x10 | 0x20, 0);
-}
+void WRITE_FND(int digit, int seg_out);
+void WRITE_FND_DOT(int digit, int seg_out);
+void FND_clear();
+void FND_init();
+void LED_init();
+#define DLY_4 	16
+#define DLY_8	8
+#define DLY_16	4
+#define DLY_32  2
+#define N1 		61068
+#define N2 		54421
+#define N3 		48484
+#define N4 		45844
+#define N5 		40815
+#define N6 		36363
+#define N7 		(int)((36363+32388)/2)
+#define N8 		30592
 
 
+void BUZZER_init();
+void TIMER_init();
 
+void delay(int count);
+void BUZZER_clear();
+void Play(int freq);
+void DelayForPlay(int DLY);
+void WDTinitISR(void);
+void PUSH_init();
+
+#endif /* MYLIB_H_ */
